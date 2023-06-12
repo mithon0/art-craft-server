ce49d63e4da785f94b141506e2cf7cf72be5e8fe
mithon0 <mithonhossen7244@gmail.com>
2023-06-12 20:57:33 +0600

    classs api create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRATE, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -25,6 +41,7 @@ const client = new MongoClient(uri, {
 });
 
 const clsassCollections = client.db("artCraftDB").collection("classes");
+const selectClassCollections = client.db("artCraftDB").collection("selectClass");
 const instructorCollections = client.db("artCraftDB").collection("instructor");
 const usersCollections = client.db("artCraftDB").collection("users");
 async function run() {
@@ -34,6 +51,12 @@ async function run() {
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post('/jwt',(req,res)=>{
+      const user =req.body
+      const token =jwt.sign(user,process.env.ACCESS_TOKEN_SECRATE,{ expiresIn: '1h' })
+      res.send({token})
+    })
 
     app.get("/class", async (req, res) => {
       const results = await clsassCollections.find().toArray();
@@ -45,7 +68,7 @@ async function run() {
     })
 
 
-    app.post('/users', async (req, res) => {
+    app.post('/users',  async(req, res) => {
       const user = req.body;
       console.log(user);
       const filter = { name: user.name, email: user.email, image: user.photoUrl };
@@ -55,6 +78,12 @@ async function run() {
         return res.send({ message: 'user already exists' })
       }
       const result = await usersCollections.insertOne(user);
+      res.send(result);
+    })
+    app.post('/selectclass',  async(req, res) => {
+      const classs = req.body;
+      
+      const result = await selectClassCollections.insertOne(classs);
       res.send(result);
     })
 
@@ -72,9 +101,7 @@ async function run() {
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
 
-      if (req.decoded.email !== email) {
-        res.send({ admin: false })
-      }
+      
 
       const query = { email: email }
       const user = await usersCollections.findOne(query);
@@ -90,7 +117,19 @@ async function run() {
           role: 'admin'
         },
       };
+      const result = await usersCollections.updateOne(filter, updateDoc);
+      res.send(result);
 
+    })
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
       const result = await usersCollections.updateOne(filter, updateDoc);
       res.send(result);
 
